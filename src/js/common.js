3171b409307d3dbd516a6450900597303fb85a30
Nathan <antnefedov33@gmail.com>
2024-10-23 16:32:04 +0300

    header & nav menu made

--- src/js/common.js
+++ src/js/common.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const hamburger = document.querySelector('.hamburger');
+const nav_list = document.querySelector('.nav_list');
+const nav_links = document.querySelectorAll('.nav_link');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	nav_list.classList.toggle('active');
+});
+
+nav_links.forEach(link => {
+	link.addEventListener('click', e => {
+		e.stopPropagation();
+		hamburger.classList.toggle('active');
+		nav_list.classList.toggle('active');
+	});
+});

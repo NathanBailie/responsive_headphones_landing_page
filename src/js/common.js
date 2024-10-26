47a2248897308261bc7b787af3cf83d0ae19a57f
Nathan <antnefedov33@gmail.com>
2024-10-26 14:16:39 +0300

    activate menu link by scrolling sections

--- src/js/common.js
+++ src/js/common.js
@@ -20,15 +20,37 @@ nav_links.forEach(link => {
 
 // scroll up
 const scrollUp = document.querySelector('.scrollup');
-window.addEventListener('scroll', () => {
-	this.scrollY >= 200
-		? scrollUp.classList.add('active')
-		: scrollUp.classList.remove('active');
-});
-
 scrollUp.addEventListener('click', function () {
 	window.scrollTo({
 		top: 0,
 		behavior: 'smooth',
 	});
+});
+
+// activate menu link by scrolling sections
+const sections = document.querySelectorAll('section[id]');
+
+window.addEventListener('scroll', () => {
+	const scrollY = this.scrollY;
+
+	sections.forEach(section => {
+		const sectionHeight = section.offsetHeight;
+		const sectionTop = section.offsetTop - 50;
+		const sectionId = section.getAttribute('id');
+
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			document
+				.querySelector('.nav_list a[href*=' + sectionId + ']')
+				.classList.add('active');
+		} else {
+			document
+				.querySelector('.nav_list a[href*=' + sectionId + ']')
+				.classList.remove('active');
+		}
+	});
+
+	// scroll up
+	this.scrollY >= 200
+		? scrollUp.classList.add('active')
+		: scrollUp.classList.remove('active');
 });

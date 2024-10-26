425ed8ee84ed6cebab6a573f1482f7a33e21c3e6
Nathan <antnefedov33@gmail.com>
2024-10-26 13:08:49 +0300

    scroll up js

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// nav menu
 const hamburger = document.querySelector('.hamburger');
 const nav_list = document.querySelector('.nav_list');
 const nav_links = document.querySelectorAll('.nav_link');
@@ -14,5 +15,20 @@ nav_links.forEach(link => {
 		e.stopPropagation();
 		hamburger.classList.toggle('active');
 		nav_list.classList.toggle('active');
+	});
+});
+
+// scroll up
+const scrollUp = document.querySelector('.scrollup');
+window.addEventListener('scroll', () => {
+	this.scrollY >= 200
+		? scrollUp.classList.add('active')
+		: scrollUp.classList.remove('active');
+});
+
+scrollUp.addEventListener('click', function () {
+	window.scrollTo({
+		top: 0,
+		behavior: 'smooth',
 	});
 });
